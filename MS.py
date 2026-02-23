892585c4de25527efc16caa75933dd4d26a71119
nmkhang <nguyenminhkhang60099@gmail.com>
2026-02-23 21:55:08 +0700

    chinh sua

--- MS.py
+++ MS.py
@@ -1,13 +1,10 @@
 def merge(arr, low, mid, high):
-    L = []
-    R = [] 
-    n_1, n_2 = mid - low + 1, high - mid
-    for i in range(n_1):
-        L.append(arr[low + i])
-    for j in range(n_2):
-        R.append(arr[mid + 1 + j]) 
-    i, j, k = 0, 0, low   
-    while i < n_1 and j < n_2:
+    L = arr[low:mid + 1]
+    R = arr[mid + 1:high + 1]
+    
+    i, j, k = 0, 0, low
+    
+    while i < len(L) and j < len(R):
         if L[i] <= R[j]:
             arr[k] = L[i]
             i += 1
@@ -15,11 +12,13 @@ def merge(arr, low, mid, high):
             arr[k] = R[j]
             j += 1
         k += 1
-    while i < n_1:
+        
+    while i < len(L):
         arr[k] = L[i]
         i += 1
         k += 1
-    while j < n_2:
+        
+    while j < len(R):
         arr[k] = R[j]
         j += 1
         k += 1
@@ -27,7 +26,6 @@ def merge(arr, low, mid, high):
 def merge_sort(arr, left, right):
     if left < right:
         mid = (left + right) // 2
-
         merge_sort(arr, left, mid)
         merge_sort(arr, mid + 1, right)
         merge(arr, left, mid, right)
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# Báo cáo hiệu năng của các thuật toán sắp xếp
-
 - **Tên sinh viên thực hiện:** Nguyễn Minh Khang
 - **MSSV:** 25520793
 - **Lớp:** TTNT2025
 
-### Kết quả so sánh hiệu năng
+### Nội dung:
 Nội dung tập trung phân tích và so sánh thời gian thực thi, độ phức tạp của các thuật toán sắp xếp với các bộ dữ liệu khác nhau
